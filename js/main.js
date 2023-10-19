cdb8f95695318adaa2e039ca3e20f4415fac8b10
Natalia Avila <2036000855@utna.edu.mx>
2023-10-18 21:25:16 -0600

    FUNCION PARA QUE LAS PALABRAS ESTEN DESPLEGANDO UNA EN SEGUIDA DE LA OTRA

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-var typed = new typed(".text",{
-    Strings:["Frontend Developer", "Makeup artist", "Web Developer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
+/*FUNCION PARA QUE LAS PALABRAS ESTEN DESPLEGANDO UNA EN SEGUIDA DE LA OTRA;*/
+
+var typewriter = $('.text');
+
+  if(typewriter.length) {
+
+    function initTypewriter() {
+
+     var typed = new Typed(".text", {
+        strings: $(".text").attr("data-typewriter").split("|").map(function(e) {
+          return e
+        }),
+        typeSpeed: 80,
+        backSpeed: 75,
+        startDelay: 1000,
+        backDelay: 2000,
+        loop: !0,
+        loopcount: false,
+        showCursor: false,
+        callback: function(e){ } // call function after typing is done
+        });
+    };
+
+    initTypewriter();
+
+  };
