e6a971bef86839c0fe67c66aa52628fca1c655b3
Natalia Avila <2036000855@utna.edu.mx>
2023-10-18 20:38:48 -0600

    Agregue funcion para letras en curso

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var typed = new typed(".text",{
-    String:["Frontend Developer", "Makeup artist", "Web Developer"],
+    Strings:["Frontend Developer", "Makeup artist", "Web Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
