d97b31c72065c2297c7c1452dab96431042ac894
Natalia Avila <2036000855@utna.edu.mx>
2023-11-29 12:04:37 -0600

    sw modificado

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ const archivosCache =[
     "img/img-Naty.jpeg",
 ]
 
-self.addEventListener('install', e =>{
+/*self.addEventListener('install', e =>{
     console.log('El service worker se instalo', e)
     e.waitUntil(
             caches.open(nombreCache).then((cache)=>{
@@ -32,3 +32,50 @@ self.addEventListener('fetch', e=> {
     })
     )
 })
+*/
+self.addEventListener('install', e => {
+    console.log("El SW se instaló", e);
+    e.waitUntil(
+        caches.open(nombreCache)
+        .then((cache) => {
+            console.log();
+            return cache.addAll(archivosCache);
+        })
+        .catch(error => {
+            console.error('Error durante la instalación del caché:', error);
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    console.log("Fetch...", e);
+    e.respondWith(
+        caches.match(e.request)
+        .then(respuestaCache => {
+            return respuestaCache || fetch(e.request)
+                .then(respuestaRed => {
+                    return caches.open(nombreCache)
+                        .then(cache => {
+                            cache.put(e.request, respuestaRed.clone());
+                            return respuestaRed;
+                        });
+                })
+                .catch(error => {
+                    console.error('Error al intentar recuperar el recurso:', error);
+                });
+        })
+    );
+});
+
+self.addEventListener('activate', e => {
+    console.log("El SW está activo", e);
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key !== nombreCache) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
