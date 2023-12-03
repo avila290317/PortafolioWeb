fc60b27b087a74bfb50f3d796d99efcae2d80c84
Natalia Avila <2036000855@utna.edu.mx>
2023-12-02 20:13:06 -0600

    Version Final

--- sw.js
+++ sw.js
@@ -7,32 +7,11 @@ const archivosCache =[
     "js/app.js",
     "js/main.js",
     "img/img-Naty.jpeg",
+    "img/apiRest.jpg",
+    "img/pwa.jpg",
+    "img/VJ.jpeg"
 ]
 
-/*self.addEventListener('install', e =>{
-    console.log('El service worker se instalo', e)
-    e.waitUntil(
-            caches.open(nombreCache).then((cache)=>{
-            console.log("Cache guardada correctamente")
-            cache.addAll(archivosCache);
-        })
-    )
-})
-
-self.addEventListener('activate', e =>{
-    console.log('Service Worker activo',e)
-})
-
-self.addEventListener('fetch', e=> {
-    console.log('Fetch.. ', e)
-    e.respondWith(
-        caches.match(e.request)
-        .then(respuestaCache =>{
-            return respuestaCache
-    })
-    )
-})
-*/
 self.addEventListener('install', e => {
     console.log("El SW se instaló", e);
     e.waitUntil(
